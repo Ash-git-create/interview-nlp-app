c140008eaf8e322d7bf181e21984f53f896b2c38
Ashwin <ashwin.achu00@gmail.com>
2025-05-09 21:18:31 +0200

    Update flan_t5_prompting.py

--- flan_t5_prompting.py
+++ flan_t5_prompting.py
@@ -1,6 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
-# Load FLAN-T5 model for question generation
 model_id = "google/flan-t5-large"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
